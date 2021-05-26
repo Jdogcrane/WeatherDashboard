d95107e548767b19cd0661ad155ce662888c6a2f
Josh <jdogcrane@gmail.com>
2021-05-26 17:05:51 -0400

    got my event lister set up for my search

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,13 @@
+// values
 var inputText = document.querySelector('.inputText');
-var button = document.querySelector('btn')
+var button = document.querySelector('.btn');
+var showData = document.querySelector('.data');
+
+// grabbing api data
+var apiKey = "58d4139f26e4fb35c4f0440bbeb79e2a";
+
+button.addEventListener('click', function() {
+var cityInput = inputText.value;
+
+console.log(cityInput)
+});
