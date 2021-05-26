763794c258fe01f69955ae6ab8a97fcfb90ec29e
Josh <jdogcrane@gmail.com>
2021-05-26 17:30:07 -0400

    found a wild way to input city into api url request

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,7 +7,15 @@ var showData = document.querySelector('.data');
 var apiKey = "58d4139f26e4fb35c4f0440bbeb79e2a";
 
 button.addEventListener('click', function() {
+    // captures input value from the search input
 var cityInput = inputText.value;
+var requestUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&APPID=${apiKey}`
 
-console.log(cityInput)
+fetch(requestUrl)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+console.log(data);
+});
 });
