4b2d48a6b772e61611c27fca2727933da8797684
Josh <jdogcrane@gmail.com>
2021-05-26 21:09:49 -0400

    dont have any more time doing readme now

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,3 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
 // values
 var inputText = document.querySelector('.inputText');
 var button = document.querySelector('.btn');
@@ -21,73 +9,64 @@ var apiKey = "58d4139f26e4fb35c4f0440bbeb79e2a";
 button.addEventListener('click', function () {
 
     var date = moment().format('MMMM Do');
-
     // captures input value from the search input
     var cityInput = inputText.value;
     // Request url modifiable by inputText 
-    var requestUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&APPID=${apiKey}`
-    var requestUrl5 = `http://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&units=imperial&APPID=${apiKey}`
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&APPID=${apiKey}`
+    var requestUrl5 = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&units=imperial&APPID=${apiKey}`
 
-
+    // Five day weather forecast displayed depending on city input (sorry this is not cleaner I had 6 hours to do this after class today...)
     fetch(requestUrl5)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-
             showData5.innerHTML =
                 `
        <ul>
-           <li>${data.list[0].dt_txt}</li>
-           <li>${data.list[0].weather[0].description}</li>
-           <li>${data.list[0].main.temp}°F</li>
-           <li>Humidity level:${data.list[0].main.humidity}</li>
-           <li>Wind Speed:${data.list[0].wind.speed}
+            <li>${data.list[0].dt_txt}</li>
+            <li>${data.list[0].weather[0].description}</li>
+            <li>${data.list[0].main.temp}°F</li>
+            <li>Humidity level:${data.list[0].main.humidity}</li>
+            <li>Wind Speed:${data.list[0].wind.speed}</li>
        </ul>
        
        <ul>
-
-       <li>${data.list[8].dt_txt}</li>
-       <li>${data.list[8].weather[0].description}</li>
-       <li>${data.list[8].main.temp}°F</li>
-       <li>Humidity level:${data.list[0].main.humidity}</li>
-       <li>Wind Speed:${data.list[8].wind.speed}
-
-       </ul>
+            <li>${data.list[8].dt_txt}</li>
+            <li>${data.list[8].weather[0].description}</li>
+            <li>${data.list[8].main.temp}°F</li>
+            <li>Humidity level:${data.list[0].main.humidity}</li>
+            <li>Wind Speed:${data.list[8].wind.speed}</li>
+        </ul>
        
        <ul>
-
-       <li>${data.list[16].dt_txt}</li>
-       <li>${data.list[16].weather[0].description}</li>
-       <li>${data.list[16].main.temp}°F</li>
-       <li>Humidity level:${data.list[0].main.humidity}</li>
-       <li>Wind Speed:${data.list[16].wind.speed}
-
-       </ul>
+            <li>${data.list[16].dt_txt}</li>
+            <li>${data.list[16].weather[0].description}</li>
+            <li>${data.list[16].main.temp}°F</li>
+            <li>Humidity level:${data.list[0].main.humidity}</li>
+            <li>Wind Speed:${data.list[16].wind.speed}</li>
+        </ul>
        
-       <ul>
-
-       <li>${data.list[24].dt_txt}</li>
-       <li>${data.list[24].weather[0].description}</li>
-       <li>${data.list[24].main.temp}°F</li>
-       <li>Humidity level:${data.list[0].main.humidity}</li>
-       <li>Wind Speed:${data.list[24].wind.speed}
-
-       </ul>
+        <ul>
+            <li>${data.list[24].dt_txt}</li>
+            <li>${data.list[24].weather[0].description}</li>
+            <li>${data.list[24].main.temp}°F</li>
+            <li>Humidity level:${data.list[0].main.humidity}</li>
+            <li>Wind Speed:${data.list[24].wind.speed}</li>
+        </ul>
        
-       <ul>
-
-       <li>${data.list[32].dt_txt}</li>
-       <li>${data.list[32].weather[0].description}</li>
-       <li>${data.list[32].main.temp}°F</li>
-       <li>Humidity level:${data.list[0].main.humidity}</li>
-       <li>Wind Speed:${data.list[32].wind.speed}
-
-       </ul>
+        <ul>
+            <li>${data.list[32].dt_txt}</li>
+            <li>${data.list[32].weather[0].description}</li>
+            <li>${data.list[32].main.temp}°F</li>
+            <li>Humidity level:${data.list[0].main.humidity}</li>
+            <li>Wind Speed:${data.list[32].wind.speed}</li>
+        </ul>
            `;
         });
 
+// Grabs the current day weather
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
@@ -101,13 +80,13 @@ button.addEventListener('click', function () {
             // filling with the 
             showData.innerHTML =
                 `
-                                <ul>
-                                    <li class="desk">${data.weather[0].description}</li>
-                                    <li class="city">${data.name}</li>
-                                    <li class="temp">${data.main.temp}°F</li>
-                                    <li>${date}</li>
-                                </ul>
-                                    `;
+                <ul>
+                <li>${data.weather[0].description}</li>
+                <li>${data.name}</li>
+                <li>${data.main.temp}°F</li>
+                <li>${date}</li>              
+                </ul>
+                `;
         });
 });
 
